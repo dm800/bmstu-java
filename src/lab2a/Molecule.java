0f4383c065abb4290f60c90810a907f2539027d7
dm800 <limonov2005@bk.ru>
2024-03-04 23:17:36 +0300

    lab2a

---
+++ src/lab2a/Molecule.java
@@ -0,0 +1,15 @@
+package lab2a;
+
+public class Molecule {
+    static private int count;
+    public Molecule () {
+        Molecule.count += 1;
+    }
+    public int getCount () {
+        return Molecule.count;
+    }
+
+    public String toString() {
+        return "Молекула";
+    }
+}
---
+++ src/lab2a/UniverseTest.java
@@ -0,0 +1,12 @@
+package lab2a;
+
+public class UniverseTest {
+    public static void main(String [] args) {
+        Universe My_Universe = new Universe(10, 300);
+        System.out.println(My_Universe);
+        System.out.println(My_Universe.Energy());
+    }
+}
+
+
+// sjfhaeu2u1h3124haf
